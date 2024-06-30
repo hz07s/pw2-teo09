881c62778d6828fca85cb692cf39ecb027eb2055
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-30 14:27:14 -0500

    Modificando el typescript de la app para agregar un constructor y una funcion para mostrar hobbies , ademas de modificar el html para que funcione con estos nuevos cambios

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -2,15 +2,28 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-dream-app';
-  name = 'Hernan Andy C. Z.'
-  email = 'hchoquehuancaz@unsa.edu.pe'
-  webpage = 'http://www.unsa.edu.pe'
+    title = 'my-dream-app';
+    name : string;
+    email : string;
+    webpage :string;
+    hobbies : string[];
+
+    constructor() {
+        console.log("Constructor is working...");
+        this.name = "Hernan Andy";
+        this.email = "hchoquehuancaz@unsa.edu.pe";
+        this.webpage = "http://www.unsa.edu.pe";
+        this.hobbies = ["Futbol", "Programacion", "Ajedrez"];
+    }
+
+    showhobbies() {
+        return true;
+    }
 }
