311e6be549d42b2ccf72e09bf2e800fa1a47a7c8
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-30 15:08:57 -0500

    Mostrando el HTML del componente hello-world en el HTML del app, esto se hizo agregando el componente creado a los imports del app

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from "./hello-world/hello-world.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HelloWorldComponent, HelloWorldComponent]
 })
 export class AppComponent {
     title = 'my-dream-app';
--- my-dream-app/src/app/hello-world/hello-world.component.ts
+++ my-dream-app/src/app/hello-world/hello-world.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hello-world',
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './hello-world.component.html',
   styleUrl: './hello-world.component.css'
 })
-export class HelloWorldComponent {
+export class HelloWorldComponent implements OnInit{
 
+    constructor() {}
+
+    ngOnInit(): void {
+        
+    }
 }
