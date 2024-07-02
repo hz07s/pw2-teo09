12ed7acc79ed730768a30370ebe563a60ae564ba
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-07-01 23:43:51 -0500

    Mostrando el Json a traves de la consola, usando DataService, HttpClient y la interfaz Post

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -4,10 +4,21 @@ import { HelloWorldComponent } from "./hello-world/hello-world.component";
 import { UserComponent } from './user/user.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { DataService } from './data.service';
+import { HttpClient } from '@angular/common/http';
 //import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
+    template: `
+    <h2>Posts</h2>
+    <ul>
+      <li *ngFor="let post of posts">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.body }}</p>
+      </li>
+    </ul>
+  `,
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
@@ -15,55 +26,63 @@ import { BrowserModule } from '@angular/platform-browser';
         UserComponent, FormsModule]
 })
 export class AppComponent {
-    name : string = "Hernan Andy";
-    age : number = 18;
-    username: string = "";
-
-    users = ['ryan', 'joe', 'cameron', 'john'];
-    actividad = false;
-
-    sayHello() {
-        alert("Hola desde app.component");
+    posts: any[] = []
+    constructor(private http: HttpClient) {
+        this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts').subscribe(posts => {
+            console.log(posts);
+        });
     }
 
-    deleteUser(user : any) {
-        for (let i=0; i<this.users.length; i++) {
-            if (user == this.users[i]){
-                this.users.splice(i,1);
-            }
-        }
-    }
 
-    addUser(newUser : any) {
-        this.users.push(newUser.value);
-        newUser.value = '';
-        newUser.focus();
-        return false;
-    }
+    // name : string = "Hernan Andy";
+    // age : number = 18;
+    // username: string = "";
 
-    title = 'my-dream-app';
+    // users = ['ryan', 'joe', 'cameron', 'john'];
+    // actividad = false;
+
+    // sayHello() {
+    //     alert("Hola desde app.component");
+    // }
+
+    // deleteUser(user : any) {
+    //     for (let i=0; i<this.users.length; i++) {
+    //         if (user == this.users[i]){
+    //             this.users.splice(i,1);
+    //         }
+    //     }
+    // }
+
+    // addUser(newUser : any) {
+    //     this.users.push(newUser.value);
+    //     newUser.value = '';
+    //     newUser.focus();
+    //     return false;
+    // }
+
+    // title = 'my-dream-app';
     //name : string;
-    email : string;
-    webpage :string;
-    hobbies : string[];
-    showHobbies : boolean;
+    // email : string;
+    // webpage :string;
+    // hobbies : string[];
+    // showHobbies : boolean;
 
-    constructor() {
-        console.log("Constructor is working...");
-        this.name = "Hernan Andy";
-        this.email = "hchoquehuancaz@unsa.edu.pe";
-        this.webpage = "http://www.unsa.edu.pe";
-        this.hobbies = ["Futbol", "Programacion", "Ajedrez"];
-        this.showHobbies = false;
-    }
+    // constructor() {
+    //     console.log("Constructor is working...");
+    //     this.name = "Hernan Andy";
+    //     this.email = "hchoquehuancaz@unsa.edu.pe";
+    //     this.webpage = "http://www.unsa.edu.pe";
+    //     this.hobbies = ["Futbol", "Programacion", "Ajedrez"];
+    //     this.showHobbies = false;
+    // }
 
-    toggleHobbies() {
-        this.showHobbies = !this.showHobbies;
-    }
+    // toggleHobbies() {
+    //     this.showHobbies = !this.showHobbies;
+    // }
 
-    newHobby(hobby: any)  {
-        this.hobbies.push(hobby.value);
-        hobby.value = "";
-        return false;
-    }
+    // newHobby(hobby: any)  {
+    //     this.hobbies.push(hobby.value);
+    //     hobby.value = "";
+    //     return false;
+    // }
 }
--- my-dream-app/src/app/app.module.ts
+++ my-dream-app/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [],
@@ -10,9 +11,9 @@ import { BrowserModule } from '@angular/platform-browser';
     BrowserModule,
     CommonModule,
     FormsModule,
-    
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: []
 })
 export class AppModule { }
